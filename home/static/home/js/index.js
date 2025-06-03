d910825d9dc887171f2e06b1708993b31b09b76a
YianXie <yian.xie@outlook.com>
2025-06-03 10:05:26 +0800

    update CSS

--- home/static/home/js/index.js
+++ home/static/home/js/index.js
@@ -1,3 +1,5 @@
+const missionStatement = "Magic club aims to spark the child-like curiosity of its members and spread the art of magic through the medium of entertaining and exciting performances.";
+const missionStatementCitation = "— Mission Statement";
 let cardActived;
 
 function getAllParents(element) {
@@ -12,7 +14,25 @@ function getAllParents(element) {
     return parents;
 }
 
+function sleep(ms) {
+    return new Promise(resolve => {
+        setTimeout(resolve, ms);
+    });
+}
+
 document.addEventListener("DOMContentLoaded", function () {
+    setTimeout(async () => {
+        const blockQuote = document.querySelector("blockquote");
+        const blockQuoteCitation = document.querySelector(".citation");
+        for (const letter of missionStatement) {
+            blockQuote.textContent += letter;
+            await sleep(7.5); // wait for 0.01 second
+        }
+        for (const letter of missionStatementCitation) {
+            blockQuoteCitation.textContent += letter;
+            await sleep(7.5);
+        }
+    }, 3000);
     document.querySelectorAll(".card").forEach(card => {
         card.addEventListener("click", function () {
             if (cardActived && cardActived !== card) {
@@ -28,10 +48,10 @@ document.addEventListener("DOMContentLoaded", function () {
     document.querySelector(".volume-controller").addEventListener("click", function () {
         const video = document.querySelector(".promotion-video");
         if (video.muted) {
-            this.src = "../../static/home/icons/volume-on.svg";
+            this.src = "../../static/home/images/volume-on.svg";
             video.muted = false;
         } else {
-            this.src = "../../static/home/icons/volume-off.svg";
+            this.src = "../../static/home/images/volume-off.svg";
             video.muted = true;
         }
     });
