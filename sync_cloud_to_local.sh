89ff70bc3d525cd12ded0eb3f7c463f6f2f837a4
YianXie <yian.xie@outlook.com>
2025-07-29 21:07:50 +0800

    update automation sync script

---
+++ sync_cloud_to_local.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Get the variables
+source .env
+
+# Get the changes from the cloud
+pg_dump $DATABASE_URL -F c -f cloud_backup.dump
+
+# Recreate the database for cleaner sync
+dropdb -U $DATABASE_USER $DATABASE_NAME
+createdb -U $DATABASE_USER $DATABASE_NAME
+
+# Restore (overwrite) the local database
+pg_restore -U $DATABASE_USER -d $DATABASE_NAME cloud_backup.dump --no-owner --no-acl
